9ee585a6bfbdb6ed5635546d3bac297dd5235c2c
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-12-06 07:18:44 -0500

    add cloudinary to installed apps

--- gramapp/settings.py
+++ gramapp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'instagram',
-    'bootsrap4'
+    'cloudinary',
+    
 ]
 
 MIDDLEWARE = [
