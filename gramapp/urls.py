6f3778b5e47d3f541eb03c81380157f98f276de3
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-12-07 07:10:27 -0500

    add instagram urls to urlpatterns

--- gramapp/urls.py
+++ gramapp/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
 from django.contrib import admin
-from django.contrib.auth import views 
+from django.urls import path
+from django.conf.urls import url, include
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'',include('instagram.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),
+         name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^', include('instagram.urls')),
 ]
