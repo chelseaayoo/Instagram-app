2e58f158fea2f1a1ebd246cb64100e4c81e3cdd7
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-12-07 08:29:18 -0500

    update files for deployment

--- gramapp/settings.py
+++ gramapp/settings.py
@@ -16,6 +16,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from decouple import config,Csv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,4 +156,8 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 
